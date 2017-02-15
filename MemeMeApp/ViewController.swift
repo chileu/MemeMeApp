50821fa3971c9565dbe3a3645072c3369bca55e4
chiyingleung1 <chiying.leung@gmail.com>
2017-02-15 17:30:00 -0500

    customize ui image picker controller delegate methods

--- MemeMeApp/ViewController.swift
+++ MemeMeApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
@@ -25,7 +25,21 @@ class ViewController: UIViewController {
     
     @IBAction func pickAnAlbumImage(sender: AnyObject) {
         let pickerController = UIImagePickerController()
+        pickerController.delegate = self
         self.present(pickerController, animated: true, completion: nil)
+    }
+    
+    // MARK: Add UIImagePickerControllerDelegate methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imagePickerView.image = image
+            imagePickerView.contentMode = .scaleAspectFill
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
 
