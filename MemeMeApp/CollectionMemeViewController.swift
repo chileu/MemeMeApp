7a7b81bfd8534da19014a2065c8e6a2ad2bbe188
chiyingleung1 <chiying.leung@gmail.com>
2017-02-16 18:02:25 -0500

    add collection and table view controllers

---
+++ MemeMeApp/CollectionMemeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionMemeViewController.swift
+//  MemeMeApp
+//
+//  Created by Chi-Ying Leung on 2/16/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionMemeViewController: UICollectionViewController {
+    
+}
---
+++ MemeMeApp/TableMemeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableMemeViewController.swift
+//  MemeMeApp
+//
+//  Created by Chi-Ying Leung on 2/16/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableMemeViewController: UITableViewController {
+    
+}
